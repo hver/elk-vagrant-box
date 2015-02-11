15c8ed6d48920cf0cfefaa5824d23f3238f91771
Hennadiy Verkh <h.verkh@traveltrex.com>
2015-02-11 17:39:39 +0100

    Better metrics generator

--- examples/generate_metrics.php
+++ examples/generate_metrics.php
@@ -14,19 +14,68 @@ $log->pushHandler($fileHandler);
 
 $faker = Faker\Factory::create();
 
-for ($i = 0; $i < 1000; $i++) {
-    $seconds    = (int)purebell(0, 3600, 600, 1);
-    $memoryPeak = (int)purebell(30000000, 33000000, 500000, 1);
-    $ms         = (int)purebell(1000, 4000, 500, 1);
+for ($i = 0; $i < 200; $i++) {
+    $seconds = purebell(-3600 * 8, 3600 * 16, 60 * 24, 1);
+    if (!$seconds < 0) {
+        continue;
+    }
+    $memoryPeak = rand(1000000, 2500000);
+    $ms         = rand(20, 50);
+    $logMessage = getLogMessage($faker, $seconds, $memoryPeak, $ms, '404_page');
+    $log->info($logMessage);
+}
+
+for ($i = 0; $i < 600; $i++) {
+    $seconds = purebell(-3600 * 12, 3600 * 12, 600 * 24, 1, 600 * 24, 1);
+    if (!$seconds < 0) {
+        continue;
+    }
+    $memoryPeak = rand(10000000, 40000000);
+    $ms         = rand(1000, 4000);
     $logMessage = getLogMessage($faker, $seconds, $memoryPeak, $ms, 'search_request');
     $log->info($logMessage);
 }
 
-for ($i = 0; $i < 1000; $i++) {
-    $seconds    = (int)purebell(0, 3600, 600, 1);
-    $memoryPeak = (int)purebell(1000000, 2000000, 100000, 1);
-    $ms         = (int)purebell(20, 50, 5, 1);
-    $logMessage = getLogMessage($faker, $seconds, $memoryPeak, $ms, '404_page');
+for ($i = 0; $i < 400; $i++) {
+    $seconds    = purebell(3600 * 12, 3600 * 36, 600 * 24, 1);
+    $memoryPeak = rand(10000000, 40000000);
+    $ms         = rand(1000, 4000);
+    $logMessage = getLogMessage($faker, $seconds, $memoryPeak, $ms, 'search_request');
+    $log->info($logMessage);
+}
+
+for ($i = 0; $i < 5; $i++) {
+    $seconds    = rand(20 * 3600, 24 * 3600);
+    $memoryPeak = rand(60000000, 160000000);
+    $ms         = rand(5000, 16000);
+    $logMessage = getLogMessage($faker, $seconds, $memoryPeak, $ms, 'search_request');
+    $log->info($logMessage);
+}
+
+for ($i = 0; $i < 800; $i++) {
+    $seconds    = purebell(3600 * 12, 3600 * 36, 500 * 24, 1);
+    $memoryPeak = rand(3000000, 6000000);
+    $ms         = rand(30, 100);
+    $logMessage = getLogMessage($faker, $seconds, $memoryPeak, $ms, 'landing_page');
+    $log->info($logMessage);
+}
+
+for ($i = 0; $i < 1200; $i++) {
+    $seconds = purebell(-3600 * 12, 3600 * 12, 600 * 24, 1);
+    if (!$seconds < 0) {
+        continue;
+    }
+    $memoryPeak = rand(3000000, 6000000);
+    $ms         = rand(30, 100);
+    $logMessage = getLogMessage($faker, $seconds, $memoryPeak, $ms, 'landing_page');
+    $log->info($logMessage);
+}
+
+for ($i = 0; $i < 500; $i++) {
+    $seconds    = rand(0, 3600 * 24);
+    $memoryPeak = rand(1500000, 3900000);
+    $ms         = rand(100, 2500);
+    $logMessage = getLogMessage($faker, $seconds, $memoryPeak, $ms, 'ajax_api');
     $log->info($logMessage);
 }
 
@@ -52,7 +101,7 @@ function purebell($min, $max, $std_deviation, $step = 1)
         $random_number = purebell($min, $max, $std_deviation);
     }
 
-    return $random_number;
+    return (int)round($random_number);
 }
 
 /**
@@ -64,6 +113,11 @@ function purebell($min, $max, $std_deviation, $step = 1)
  */
 function getLogMessage(Faker\Generator $faker, $seconds, $memoryPeak, $ms, $type)
 {
+    if (rand(0, 10) == 4) {
+        $userAgent = 'Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)';
+    } else {
+        $userAgent = $faker->userAgent;
+    }
     @$template = [
         "@timestamp"        => date('Y-m-d\TH:i:s.uP', time() - $seconds),
         '@version'          => 1,
@@ -77,7 +131,7 @@ function getLogMessage(Faker\Generator $faker, $seconds, $memoryPeak, $ms, $type
         "http_method"       => "GET",
         "server"            => "localhost",
         "referrer"          => null,
-        "user_agent"        => $faker->userAgent,
+        "user_agent"        => $userAgent,
         "memory_peak_usage" => $memoryPeak,
         "c_request_type"    => $type,
         "c_request_time_ms" => $ms
