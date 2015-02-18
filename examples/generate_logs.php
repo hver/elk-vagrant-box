82f0e68467b2097f3fa9c59f5dd57c902d0cdfc7
Hennadiy Verkh <h.verkh@traveltrex.com>
2015-02-19 00:43:04 +0100

    Generate logs for 24 hours.

--- examples/generate_logs.php
+++ examples/generate_logs.php
@@ -14,26 +14,42 @@ $log->pushHandler($fileHandler);
 
 $faker = Faker\Factory::create();
 
-for ($i = 0; $i < 1000; $i++) {
-    $seconds    = (int)purebell(0, 3600, 600, 1);
+for ($i = 0; $i < 600; $i++) {
+    $seconds    = (int) purebell(3600 * 8, 3600 * 32, 500 * 24, 1);
+    $levelIso   = getRandomLogLevel();
+    $logMessage = getLogMessage($faker, $seconds, $levelIso, 'Logger.Main');
+    $log->$levelIso($logMessage);
+}
+
+for ($i = 0; $i < 900; $i++) {
+    $seconds = purebell(-3600 * 8, 3600 * 16, 600 * 24, 1);
+    if (!$seconds < 0) {
+        continue;
+    }
     $levelIso   = getRandomLogLevel();
     $logMessage = getLogMessage($faker, $seconds, $levelIso, 'Logger.Main');
     $log->$levelIso($logMessage);
 }
 
 for ($i = 0; $i < 100; $i++) {
-    $seconds    = (int)purebell(3000, 3300, 50, 1);
+    $seconds    = (int) purebell(3000 * 24, 3300 * 24, 50 * 24, 1);
     $levelIso   = getRandomLogLevel();
-    $logMessage = getLogMessage($faker, $seconds, $levelIso, 'Logger.Translator');
+    $logMessage = getLogMessage($faker, $seconds, $levelIso,
+      'Logger.Translator');
     $log->$levelIso($logMessage);
 }
 
 for ($i = 0; $i < 40; $i++) {
-    $seconds    = (int)purebell(1000, 1060, 10, 1);
+    $seconds    = (int) purebell(1000 * 24, 1200 * 24, 20 * 24, 1);
     $levelIso   = 'error';
-    $logMessage = getLogMessage($faker, $seconds, $levelIso, 'Logger.Translator');
+    $logMessage = getLogMessage($faker, $seconds, $levelIso,
+      'Logger.Translator');
     $log->$levelIso($logMessage);
 }
+
+echo 'Log messages generated.
+ Please wait a minute for ELK stack to process them (Vagrant makes it slow). ';
+
 /**
  * @return string
  */
@@ -72,8 +88,8 @@ function getRandomLogLevel()
  */
 function purebell($min, $max, $std_deviation, $step = 1)
 {
-    $rand1           = (float)mt_rand() / (float)mt_getrandmax();
-    $rand2           = (float)mt_rand() / (float)mt_getrandmax();
+    $rand1           = (float) mt_rand() / (float) mt_getrandmax();
+    $rand2           = (float) mt_rand() / (float) mt_getrandmax();
     $gaussian_number = sqrt(-2 * log($rand1)) * cos(2 * M_PI * $rand2);
     $mean            = ($max + $min) / 2;
     $random_number   = ($gaussian_number * $std_deviation) + $mean;
@@ -81,6 +97,7 @@ function purebell($min, $max, $std_deviation, $step = 1)
     if ($random_number < $min || $random_number > $max) {
         $random_number = purebell($min, $max, $std_deviation);
     }
+
     return $random_number;
 }
 
@@ -94,22 +111,23 @@ function purebell($min, $max, $std_deviation, $step = 1)
 function getLogMessage(Faker\Generator $faker, $seconds, $levelIso, $channel)
 {
     @$template = [
-        "@timestamp"  => date('Y-m-d\TH:i:s.uP', time() - $seconds),
-        "@version"    => 1,
-        "host"        => "vagrant-ubuntu-trusty-64",
-        "message"     => $faker->catchPhrase,
-        "type"        => "logstash_example",
-        "channel"     => $channel,
-        "level"       => strtoupper($levelIso),
-        "url"         => "www.example.com/{$faker->state}/{$faker->firstName}",
-        "ip"          => $faker->ipv4,
-        "http_method" => "GET",
-        "server"      => "localhost",
-        "referrer"    => null,
-        "user_agent"  => $faker->userAgent,
-        "file"        => "/logstash_example.php",
-        "line"        => 120
+      "@timestamp"  => date('Y-m-d\TH:i:s.uP', time() - $seconds),
+      "@version"    => 1,
+      "host"        => "vagrant-ubuntu-trusty-64",
+      "message"     => $faker->catchPhrase,
+      "type"        => "logstash_example",
+      "channel"     => $channel,
+      "level"       => strtoupper($levelIso),
+      "url"         => "www.example.com/{$faker->state}/{$faker->firstName}",
+      "ip"          => $faker->ipv4,
+      "http_method" => "GET",
+      "server"      => "localhost",
+      "referrer"    => null,
+      "user_agent"  => $faker->userAgent,
+      "file"        => "/logstash_example.php",
+      "line"        => 120
     ];
+
     return json_encode($template);
 }
 
