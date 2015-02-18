df632eadcf2fa30949604c2b8dc84e9cbb815a20
Hennadiy Verkh <h.verkh@traveltrex.com>
2015-02-18 01:03:50 +0100

    Fix from peer review

--- examples/monolog_example.php
+++ examples/monolog_example.php
@@ -8,7 +8,7 @@ use Monolog\Logger;
 $log = new Logger('Logger.Example');
 
 // To output in the console we use a handler which writes to stream.
-// And as a stream address we use "php://stdout". In a CLI php application,
+// And as a stream address we use "php://stdout". In a CLI php application
 // everything written to this stream is printed in the console.
 
 // This handler only log messages with importance levels higher than Logger::NOTICE
@@ -21,5 +21,5 @@ $log->pushHandler($cliHandler);
 $log->error('Bad data');
 // First parameter is the message, second is an array with additional info
 $log->warning('Processing took too long', ['time' => time()]);
-// Because of the minimal log level, this debug will be ignored
-$log->debug('Who reads debug log?');
+// Because of the minimal log level this debug will be ignored
+$log->debug('Who reads the debug log?');
